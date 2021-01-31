2d92dfd887c7041b80d36b260152fe87847644e5
Jose Campos <jose.campos@aggiemail.usu.edu>
2021-01-31 00:32:17 -0600

    lesson 2 challenge 1

---
+++ lesson02/calculator.js
@@ -0,0 +1,19 @@
+/**Variable 1**/
+const var1 = 'welcome';
+/**Variable 2**/
+const var2 = 'home';
+/**Variable 3**/
+const var3 = 7;
+/**Variable 4**/
+const var4 = 13;
+/**Variable 5**/
+const var5 = 2;
+/**Operation 1**/
+oper1 = var1 + ' ' + var2;
+console.log(oper1)
+/**Operation 2**/
+oper2 = (var3 + var4)*var5 ;
+console.log(oper2)
+/**Operation 3**/
+oper3 = ((var3**2) + var4)/var5;
+console.log(oper3)
