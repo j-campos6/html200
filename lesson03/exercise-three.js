646e5ff6e70d13252fbf76c1ca5dcefbad1547f6
Jose Campos <jose.campos@aggiemail.usu.edu>
2021-01-31 21:34:51 -0600

    lesson 3 challenge 3

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,7 @@
+/**Array with five strings**/
+const colors = ['blue','yellow','purple','brown','white'];
+
+/**Loop with console log of each element w/index and comma**/
+for (let i = 0; i < colors.length; i++) {
+  console.log(i + ", " + colors[i])
+}
