36641574c07ac2f8e9afadd6dd46acd866deb829
Jose Campos <jose.campos@aggiemail.usu.edu>
2021-01-31 21:15:22 -0600

    lesson 3 challenge 2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,8 @@
+/**String Variable (sentence of 5words)**/
+const x = "All the leaves are brown.";
+
+/**Array from sentence**/
+const sentence5 = x.split(' ');
+
+/**Console log of third word**/
+console.log('third word is:' + ' ' + sentence5[2])
